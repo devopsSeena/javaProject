a01b69bfa5803b32a5cc66fd099acbbdee0a085b
EC2 Default User <ec2-user@ip-172-31-88-251.ec2.internal>
2021-11-17 15:49:49 +0000

    trigger example poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,4 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-System.out.println("Hello, World");
-
-  }
+    System.out.println("Hello, World"); }
 }
